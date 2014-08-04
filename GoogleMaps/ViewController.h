024cab3c635545d784f55e06a6dac55dd9a1267a
Adam <ajgask@gmail.com>
2014-08-04 12:19:45 +0100

    Initial Commit

---
+++ GoogleMaps/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GoogleMaps
+//
+//  Created by Adam Gask on 04/08/2014.
+//  Copyright (c) 2014 AJ9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GoogleMapsTests/GoogleMapsTests.m
@@ -0,0 +1,34 @@
+//
+//  GoogleMapsTests.m
+//  GoogleMapsTests
+//
+//  Created by Adam Gask on 04/08/2014.
+//  Copyright (c) 2014 AJ9. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GoogleMapsTests : XCTestCase
+
+@end
+
+@implementation GoogleMapsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
